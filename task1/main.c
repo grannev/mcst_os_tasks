a37407a1eccfaa44c29a34384319f9973ba6ff4b
grannev <eqorrannev@gmail.com>
2026-03-30 23:24:17 +0400

    task1 | feat: merge sort remaining sorted parts feat: using dynarr instead of simple array

--- task1/main.c
+++ task1/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
+
+#include "dynarr.h"
 
 
 int round_div(int x, int y)
@@ -63,22 +64,53 @@ void *bubble_sort(void *args)
 }
 
 
+void merge(int *array, int left, int mid, int right)
+{
+    int n = mid - left + 1, m =  right - mid, i, j, k;
+    int *pleft = malloc(sizeof(int) * n);
+    int *pright = malloc(sizeof(int) * m);
+
+    for (i = 0; i < n; i++)
+        pleft[i] = array[left + i];
+    for (i = 0; i < m; i++)
+        pright[i] = array[mid + i + 1];
+
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n && j < m) {
+        if (pleft[i] <= pright[j])
+            array[k++] = pleft[i++];
+        else
+            array[k++] = pright[j++];
+    }
+    while (i < n)
+        array[k++] = pleft[i++];
+    while (j < m)
+        array[k++] = pright[j++];
+
+    free(pleft);
+    free(pright);
+}
+
+
 int main(int argc, char *argv[])
 {
     if (argc < 2)
         return 1;
     
-    int size = 10;
     int threads_amount = cstr_to_int(argv[1]);
-    int part_size, i, left, right;
+    int part_size, i, left, mid, right, curr, tmp, size;
 
-    if (argc == 3)
-        size = cstr_to_int(argv[2]);
-    int *array = malloc(sizeof(int) * size);
-    for (i = 0; i < size; i++)
-        array[i] = size - i;
-    print_array(array, size);
+    // init array
+    dynarr dyn;
+    init_dynarr(&dyn);
+    while (scanf("%d", &tmp) == 1)
+        push_back_dynarr(&dyn, tmp);
+    //print_array(dyn.array, dyn.size);
+    size = dyn.size;
 
+    // init threads
     part_size = (size + threads_amount - 1) / threads_amount;
     pthread_t *threads = malloc(sizeof(pthread_t) * threads_amount);
     entry *entries = malloc(sizeof(entry) * threads_amount);
@@ -89,16 +121,30 @@ int main(int argc, char *argv[])
             right = size - 1;
         if (left >= size)
             continue;
-        init_entry(entries + i, array, left, right);
+        init_entry(entries + i, dyn.array, left, right);
         pthread_create(threads + i, NULL, bubble_sort, (void*)(entries + i));
     }
 
+    // wait for threads
     for (i = 0; i < threads_amount; i++)
         if (i * part_size < size)
             pthread_join(threads[i], NULL);
-    print_array(array, size);
+    //print_array(dyn.array, size);
+   
+    // merge sorted parts
+    for (curr = part_size; curr < size; curr *= 2) {
+        for (left = 0; left < size; left += 2*curr) {
+            mid = left + curr - 1;
+            tmp = left + 2 * curr - 1;
+            right = tmp < size ? tmp : size - 1;
+            if (mid < right)
+                merge(dyn.array, left, mid, right);
+        }
+    }
     
-    free(array);
+    print_array(dyn.array, size);
+    
+    free(dyn.array);
     free(threads);
     free(entries);
 
