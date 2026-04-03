c4eead05379870578ad6bb30a7e853f737b814e3
grannev <eqorrannev@gmail.com>
2026-04-03 10:38:52 +0400

    task2 | feat: loop process of reading and writing data task* | docs: add comments to every subprogram

--- task2/main.c
+++ task2/main.c
@@ -12,10 +12,11 @@ int main(int argc, char *argv[])
         return 1;
 
     const char *fifo_name = argv[1];
-    int status = mkfifo(fifo_name, 0666);
+    mkfifo(fifo_name, 0666); // creating fifo if user doesnt
 
     int fifo_stream, write_flag = 1;
     fifo_stream = open(fifo_name, O_WRONLY | O_NONBLOCK, 0666);  
+    // if no one open to read, returns -1
     if (fifo_stream == -1) {
         fifo_stream = open(fifo_name, O_RDONLY, 0666);
         write_flag = 0;
@@ -23,24 +24,36 @@ int main(int argc, char *argv[])
         close(fifo_stream);
         fifo_stream = open(fifo_name, O_WRONLY, 0666);
     }
- 
+
+    // buffers for data
     const int input_size = 1024;
     const int output_size = 1024;
-    int read_status, write_status;
     char input[input_size] = {};
     char output[output_size] = {};
-
-    if (write_flag) {
-        printf(">");
-        fgets(input, input_size, stdin);
-        input[find_symb_cstr(input, '\n')] = '\0';
-        write_status = write(fifo_stream, input, input_size);
-        printf("Write %d bytes\n", write_status);
-    } else {
-        read_status = read(fifo_stream, output, output_size);
-        printf("Read %d bytes\n< %s\n", read_status, output);
-    } 
- 
+    
+    /* According to write_flag chosing what action we need to do now,
+     * after reading or writing data, close fifo_stream and
+     * open for read or write respectively */
+    while (!cmp_cstr(input, "--quit")) {
+        if (write_flag) {
+            //printf("<");
+            fgets(input, input_size, stdin);
+            input[find_symb_cstr(input, '\n')] = '\0';
+            write(fifo_stream, input, input_size);
+        } else {
+            read(fifo_stream, output, output_size);
+            //printf(">");
+            printf("%s\n", output);
+        }
+        clr_cstr(input);
+        clr_cstr(output);
+        close(fifo_stream);
+        write_flag = !write_flag;
+        if (write_flag)
+            fifo_stream = open(fifo_name, O_WRONLY, 0666);
+        else
+            fifo_stream = open(fifo_name, O_RDONLY, 0666);
+    }
 
     return 0;
 }
